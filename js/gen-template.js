9524159704c9f509440cd9b7a772e462ab409e27
Vkhv73 <v.dmitry.v@gmail.com>
2021-07-05 00:55:24 +0500

    исправляет работу с картой и генерацию данных для попапов меток

--- js/gen-template.js
+++ js/gen-template.js
@@ -1,5 +1,5 @@
-import { createNearestPlaces } from './data.js';
-// import { typeOfApartmentObj } from './data.js';
+// import { createRandomNearestPlaces } from './data.js';
+import { typeOfApartmentObj } from './data.js';
 
 const cardNotification = document
   .querySelector('#card')
@@ -9,8 +9,6 @@ const cardNotification = document
 
 // const cardNotificationСanvas = document.querySelector('.map__canvas'); // нахожу, куда буду передавать заполненные данными шаблоны
 // cardNotificationСanvas.classList.add('visually-hidden');
-
-const similarCards = createNearestPlaces();
 
 function fillFeatures(featuresElement, featuresArray) {
   featuresElement.innerHTML = '';
@@ -23,7 +21,6 @@ function fillFeatures(featuresElement, featuresArray) {
   });
 }
 
-// const photosDiv = cardNotification.querySelector('.popup__photos');
 const photosObj = cardNotification.querySelector('.popup__photo');
 
 function fillImages(photosElement, photosArray) {
@@ -37,64 +34,59 @@ function fillImages(photosElement, photosArray) {
   });
 }
 
-// similarCards.forEach((item) => {
-//   const cardNotificationClone = cardNotification.cloneNode(true);
+const createPopup = ( item ) => {
+  const cardNotificationClone = cardNotification.cloneNode(true);
 
-//   item.offer.title !== undefined
-//     ? cardNotificationClone.querySelector('.popup__title').textContent =
-//     item.offer.title
-//     : cardNotificationClone.querySelector('.popup__title').remove();
+  item.offer.title !== undefined
+    ? cardNotificationClone.querySelector('.popup__title').textContent =
+    item.offer.title
+    : cardNotificationClone.querySelector('.popup__title').remove();
 
-//   item.offer.address !== undefined
-//     ? cardNotificationClone.querySelector('.popup__text--address').textContent =
-//     item.offer.address
-//     : cardNotificationClone.querySelector('.popup__text--address').remove();
+  item.offer.address !== undefined
+    ? cardNotificationClone.querySelector('.popup__text--address').textContent =
+    item.offer.address
+    : cardNotificationClone.querySelector('.popup__text--address').remove();
 
-//   item.offer.price !== undefined
-//     ? cardNotificationClone.querySelector(
-//       '.popup__text--price').textContent = `${item.offer.price} ₽/ночь`
-//     : cardNotificationClone.querySelector(
-//       '.popup__text--price').remove();
-//   item.offer.type !== undefined
-//     ? cardNotificationClone.querySelector('.popup__type').textContent = typeOfApartmentObj[item.offer.type]
-//     : cardNotificationClone.querySelector('.popup__type').remove();
-
-
-//   item.offer.rooms === undefined || item.offer.guests === undefined
-//     ? cardNotificationClone.querySelector(
-//       '.popup__text--capacity').remove()
-//     : cardNotificationClone.querySelector(
-//       '.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`;
-
-//   item.offer.checkin === undefined || item.offer.checkout === undefined
-//     ? cardNotificationClone.querySelector(
-//       '.popup__text--time').remove()
-//     : cardNotificationClone.querySelector(
-//       '.popup__text--time').textContent = `Заезд после ${item.offer.checkin}, выезд до ${item.offer.checkout}`;
-
-//   item.offer.features !== undefined
-//     ? fillFeatures(cardNotificationClone.querySelector('.popup__features'), item.offer.features)
-//     : cardNotificationClone.querySelector('.popup__features').remove();
-
-//   item.offer.photos !== undefined
-//     ? fillImages(cardNotificationClone.querySelector('.popup__photos'), item.offer.photos)
-//     : cardNotificationClone.querySelector('.popup__photos').remove();
-
-//   item.offer.description !== undefined
-//     ? cardNotificationClone.querySelector('.popup__description').textContent = item.offer.description
-//     : cardNotificationClone.querySelector('.popup__description').remove();
-
-//   item.author.avatar !== undefined
-//     ? cardNotificationClone.querySelector('.popup__avatar').src = item.author.avatar
-//     : cardNotificationClone.querySelector('.popup__avatar').remove();
+  item.offer.price !== undefined
+    ? cardNotificationClone.querySelector(
+      '.popup__text--price').textContent = `${item.offer.price} ₽/ночь`
+    : cardNotificationClone.querySelector(
+      '.popup__text--price').remove();
+  item.offer.type !== undefined
+    ? cardNotificationClone.querySelector('.popup__type').textContent = typeOfApartmentObj[item.offer.type]
+    : cardNotificationClone.querySelector('.popup__type').remove();
 
 
-//   // cardNotificationСanvas.appendChild(cardNotificationClone);
-//   // console.log(cardNotificationClone);
-// });
+  item.offer.rooms === undefined || item.offer.guests === undefined
+    ? cardNotificationClone.querySelector(
+      '.popup__text--capacity').remove()
+    : cardNotificationClone.querySelector(
+      '.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`;
 
-export { cardNotification };
-export { similarCards };
-export { fillFeatures, fillImages };
+  item.offer.checkin === undefined || item.offer.checkout === undefined
+    ? cardNotificationClone.querySelector(
+      '.popup__text--time').remove()
+    : cardNotificationClone.querySelector(
+      '.popup__text--time').textContent = `Заезд после ${item.offer.checkin}, выезд до ${item.offer.checkout}`;
 
+  item.offer.features !== undefined
+    ? fillFeatures(cardNotificationClone.querySelector('.popup__features'), item.offer.features)
+    : cardNotificationClone.querySelector('.popup__features').remove();
+
+  item.offer.photos !== undefined
+    ? fillImages(cardNotificationClone.querySelector('.popup__photos'), item.offer.photos)
+    : cardNotificationClone.querySelector('.popup__photos').remove();
+
+  item.offer.description !== undefined
+    ? cardNotificationClone.querySelector('.popup__description').textContent = item.offer.description
+    : cardNotificationClone.querySelector('.popup__description').remove();
+
+  item.author.avatar !== undefined
+    ? cardNotificationClone.querySelector('.popup__avatar').src = item.author.avatar
+    : cardNotificationClone.querySelector('.popup__avatar').remove();
+
+  return cardNotificationClone;
+};
+
+export { createPopup };
 
--- js/map-handler.js
+++ js/map-handler.js
@@ -1,14 +1,13 @@
 import { form, mapFilters, removeDisabledForms } from './activity-mode-switch.js';
-// import { noticeInputAddress } from './form.js';
 
-// import { createNearestPlaces } from './data.js';
-import { typeOfApartmentObj } from './data.js';
-import { similarCards } from './gen-template.js';
-import { cardNotification } from './gen-template.js';
-import { fillFeatures, fillImages} from './gen-template.js';
+import { createRandomNearestPlaces } from './data.js';
+import { createPopup } from './gen-template.js';
 
 
 const address = document.querySelector('#address');
+// Создание массива из 10ти рандомных обхектов
+// Потом будем получать данные с сервера
+const similarCards = createRandomNearestPlaces();
 
 const LAT_CENTRE = 35.6895;
 const LNG_CENTRE = 139.692;
@@ -31,6 +30,12 @@ L.tileLayer(
   },
 ).addTo(map);
 
+const pinIcon = L.icon({
+  iconUrl: '../img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
 const mainPinIcon = L.icon({
   iconUrl: '../img/main-pin.svg',
   iconSize: [52, 52],
@@ -49,8 +54,8 @@ const mainPinMarker = L.marker(
   },
 
 );
-mainPinMarker.addTo(map).bindPopup(mainPinMarker.title); //! Уточнить у Павла, почему не передаётся значение и затем удалить балун для этой метки
-
+mainPinMarker.addTo(map);
+// mainPinMarker.bindPopup(mainPinMarker.title); //! +++ Уточнить у Павла, почему не передаётся значение и затем удалить балун для этой метки
 
 mainPinMarker.on('moveend', (evt) => {
   const markerPosition = (evt.target.getLatLng());
@@ -60,7 +65,38 @@ mainPinMarker.on('moveend', (evt) => {
   return markerPosition;
 });
 
+const markerGroup = L.layerGroup().addTo(map);
+
+const createPointsMap = function (arraySimilarCards) {
+  arraySimilarCards.forEach((item) => {
+    const { lat, lng } = item.location;
+
+    const marker = L.marker(
+      {
+        lat: lat, // item.location.lat
+        lng: lng,
+      },
+      {
+        icon: pinIcon,
+      },
+    );
+
+    marker
+      .addTo(markerGroup)
+      .bindPopup(createPopup(item), // Результат createPopup => htmlelement article
+        {
+          keepInView: true,
+        });
+  });
+};
+
+createPointsMap(similarCards);
+
 const resetMap = () => {
+  markerGroup.clearLayers(); // очистит и не добавит
+
+  createPointsMap(similarCards); // а эта добавит
+
   mainPinMarker.setLatLng({
     lat: LAT_CENTRE,
     lng: LNG_CENTRE,
@@ -72,89 +108,6 @@ const resetMap = () => {
     lat: LAT_CENTRE,
     lng: LNG_CENTRE,
   }, 12);
-}; //! КАК ЗАКРЫВАТЬ ОТКРЫТЫЕ СООБЩЕНИЯ, ПРИ НАЖАТИИ КНОПКИ РЕЗЕТ, СПРОСИТЬ У ПАВЛА?
+};
 
 export { map, mainPinMarker, resetMap }; //! убрать потом вниз файла СНАЧАЛА СПРОСИТЬ У ПАВЛЯ ЗАЧЕМ МЫ ЭТО ЭКСПОРТИРОВАЛИ
-
-//!---------- попробую получить данные для группы меток и создать эти метки
-// const simulatedData = createNearestPlaces(); //? ВООБЩЕ РАЗОБРАТЬСЯ ЧТО УБРАТЬ, ЧТО ОСТАВИТЬ ПРОКОНСУЛЬТИРОВАТЬСЯ С ПАВЛОМ
-// console.log(simulatedData);
-// console.log(similarCards);
-// console.log(cardNotification);
-
-//! НИЖЕ ИСПОЛЬЗУЮ КОД ИЗ ФАЙЛА GEN-TEMPLATE.. УЗНАТЬ У ПАВЛА, КАК ПРАВИЛЬНО, СЮДА ТЯНУТЬ, ИЛИ ОТ СЮДА ПЕРЕДАТЬ?
-similarCards.forEach((item) => {
-  const newCardClone = cardNotification.cloneNode(true);
-
-  const { lat, lng } = item.location;
-  // const offer = point.offer;
-
-  const icon = L.icon({
-    iconUrl: '../img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-
-  const marker = L.marker(
-    {
-      lat,
-      lng,
-    },
-    {
-      icon,
-    },
-  );
-
-  item.offer.title !== undefined
-    ? newCardClone.querySelector('.popup__title').textContent =
-    item.offer.title
-    : newCardClone.querySelector('.popup__title').remove();
-
-  item.offer.address !== undefined
-    ? newCardClone.querySelector('.popup__text--address').textContent =
-    item.offer.address
-    : newCardClone.querySelector('.popup__text--address').remove();
-
-  item.offer.price !== undefined
-    ? newCardClone.querySelector(
-      '.popup__text--price').textContent = `${item.offer.price} ₽/ночь`
-    : newCardClone.querySelector(
-      '.popup__text--price').remove();
-  item.offer.type !== undefined
-    ? newCardClone.querySelector('.popup__type').textContent = typeOfApartmentObj[item.offer.type]
-    : newCardClone.querySelector('.popup__type').remove();
-
-
-  item.offer.rooms === undefined || item.offer.guests === undefined
-    ? newCardClone.querySelector(
-      '.popup__text--capacity').remove()
-    : newCardClone.querySelector(
-      '.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`;
-
-  item.offer.checkin === undefined || item.offer.checkout === undefined
-    ? newCardClone.querySelector(
-      '.popup__text--time').remove()
-    : newCardClone.querySelector(
-      '.popup__text--time').textContent = `Заезд после ${item.offer.checkin}, выезд до ${item.offer.checkout}`;
-
-  item.offer.features !== undefined
-    ? fillFeatures(newCardClone.querySelector('.popup__features'), item.offer.features)
-    : newCardClone.querySelector('.popup__features').remove();
-
-  item.offer.photos !== undefined
-    ? fillImages(newCardClone.querySelector('.popup__photos'), item.offer.photos)
-    : newCardClone.querySelector('.popup__photos').remove();
-
-  item.offer.description !== undefined
-    ? newCardClone.querySelector('.popup__description').textContent = item.offer.description
-    : newCardClone.querySelector('.popup__description').remove();
-
-  item.author.avatar !== undefined
-    ? newCardClone.querySelector('.popup__avatar').src = item.author.avatar
-    : newCardClone.querySelector('.popup__avatar').remove();
-
-  marker
-    .addTo(map)
-    .bindPopup(newCardClone);
-});
-
